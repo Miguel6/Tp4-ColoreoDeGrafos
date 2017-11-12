c569d4c2f8706ec761cffbc15b7ae955df5b9970
Cristian <castagna.cristian16@gmail.com>
2017-11-12 10:04:16 -0300

    Se agregan metodos a la clase Nodo
    Se la hace Comparable
    se hace override del toString
    getter & setters de color y grado

--- src/nodo/Nodo.java
+++ src/nodo/Nodo.java
@@ -4,13 +4,15 @@ package nodo;
  * Clase nodo, toda la informacion necesaria del nodo:
  *	Color y el valor del mismo
  */
-public class Nodo {
+public class Nodo implements Comparable{
 	private int nodo;
 	private int color;
+	private int grado;
 
 	public Nodo(int nodo) {
 		this.nodo=nodo;
 		this.color = 0;
+		this.grado = 0;
 	}
 	public Nodo(int nodo, int color) {
 		this.nodo = nodo;
@@ -24,5 +26,25 @@ public class Nodo {
 	public int getColor() {
 		return color;
 	}
-
+	
+	public void setColor(final int color) {
+		this.color = color;
+	}
+	public int getGrado() {
+		return grado;
+	}
+	public void setGrado(int grado) {
+		this.grado = grado;
+	}
+	
+	@Override
+	public int compareTo(Object obj) {
+		Nodo otroNodo = (Nodo) obj;
+		return this.grado - otroNodo.getGrado() ;
+	}
+	
+	@Override
+	public String toString() {
+		return ("nodo: " + this.nodo + "		color: " + this.color + "	grado: " + this.grado);
+	}
 }
