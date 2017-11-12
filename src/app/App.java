557c2b825765e03b9628a1b46d6afd7e13d9086f
Miguel Angel Amengual <amengualma6@gmail.com>
2017-11-12 19:28:36 -0300

    PrimeraPrueba

--- src/app/App.java
+++ src/app/App.java
@@ -1,20 +1,25 @@
 package app;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 import exception.AristaException;
 import exception.GrafoException;
 import exception.PorcentajeDeAdyacenciaException;
+import tareasPreliminares.Coloreo;
 import tareasPreliminares.GeneradorDeGrafos;
-import tareasPreliminares.Grafo;
 
 public class App {
 
 
 	public static void main(String[] args) throws IOException, AristaException, GrafoException, PorcentajeDeAdyacenciaException {
-		GeneradorDeGrafos gdg = new GeneradorDeGrafos(7);
-		Grafo g = gdg.generarGrafoNPartito(4);
-		g.mostrarMatriz();
+		GeneradorDeGrafos gdg = new GeneradorDeGrafos(600);
+		LinkedList<Integer> colores = new LinkedList<>();
+		for(int i=0;i<10000;i++) {
+			colores.add(Coloreo.matula(gdg.GenerarGrafoPorcentajeAdyacencia(0.4)).getCantidadColores());
+			System.out.println(colores.get(i));
+		}
+		
 		
 	}
 }
